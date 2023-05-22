10feba2e0745999be4c294ce95fb5b64f6e656b9
9rodrigosoares <a81120476>
2023-05-22 01:37:11 -0300

    special move promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,10 +5,7 @@ import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
-import java.util.ArrayList;
-import java.util.InputMismatchException;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Program {
     public static void main(String[] args) {
@@ -37,6 +34,16 @@ public class Program {
 
                 if (capturedPiece != null) {
                     captured.add(capturedPiece);
+                }
+
+                if (chessMatch.getPromoted() != null) {
+                    System.out.print("Enter piece for promotion (B/N/R/Q): ");
+                    String type = input.nextLine().toUpperCase(Locale.ROOT);
+                    while (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
+                        System.out.print("Invalid value! Enter piece for promotion (B/N/R/Q): ");
+                        type = input.nextLine().toUpperCase(Locale.ROOT);
+                    }
+                    chessMatch.replacePromotedPiece(type);
                 }
 
             } catch (ChessException e) {
